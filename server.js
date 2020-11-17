34d67ce2b1c3029faff22d571c4ac68edd42bdc1
Anmol <mr.anmolthapar@gmail.com>
2020-11-18 11:41:38 +1300

    hope

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use((req,res,next) =>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
-});
+// app.use((req,res,next) =>{
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Headers", "*");
+//     next();
+// });
 // support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
