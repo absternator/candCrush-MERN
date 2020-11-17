5475324db76641dd214e9412b9a7ee4bd39bed8b
Anmol <mr.anmolthapar@gmail.com>
2020-11-18 11:52:40 +1300

    pray

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use((req,res,next) =>{
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    next();
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  next();
 });
 // support parsing of application/json type post data
 app.use(bodyParser.json());
@@ -35,19 +35,21 @@ connection.once("open", () => {
 const scoresRouter = require("./routes/scores");
 //use routes from scores
 app.use("/scores", scoresRouter);
-
+app.get("/", (req, res) => {
+  res.send("Wpwww");
+});
 /***********Configure front end********* */
 // "heroku-postbuild" : "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
 // if in production
-if (process.env.NODE_ENV === "production") {
-  //serve up static files
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   //serve up static files
+//   app.use(express.static("client/build"));
 
-  // connect index.html
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+//   // connect index.html
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 // listen om port 5000
 const port = process.env.PORT || 5000;
